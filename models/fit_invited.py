b86a8edc2f41f1710d2542eaf0d3ddbe50511e43
ZacLima <zacarias.lima@intitecperu.com>
2018-07-20 13:17:30 -0500

    proyecto contac

--- models/fit_invited.py
+++ models/fit_invited.py
@@ -4,4 +4,5 @@ from odoo import models, fields
 
 class InvitadoInvited(models.Model):
     _inherit = 'helpdesk.tag'
+
     invited_ids = fields.Many2many('res.partner', string='Seguidores', ondelete='cascade')
